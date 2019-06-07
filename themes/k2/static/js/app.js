c73a8c792651147c4f8b32f525ca61b167b30849
Paul M Fox <iam@yourheropaul.com>
2019-06-07 14:18:29 +0100

    basic layout and favicon

--- themes/k2/static/js/app.js
+++ themes/k2/static/js/app.js
@@ -1,4 +1,9 @@
 (function() {
 
+    let calculateHeight = () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    calculateHeight();
 
 })(); 
